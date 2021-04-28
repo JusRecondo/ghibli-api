069b352d6588dccee3205b6aafe549c244cc0e4a
JusRecondo <justina.recondo@gmail.com>
2021-04-27 22:41:47 -0300

    inicializacion de repo local

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* API de Studio Ghibli
+https://ghibliapi.herokuapp.com/
+
+ENDPOINTS
+
+Películas
+https://ghibliapi.herokuapp.com/films
+
+Acceder a una película en particular
+https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6
+
+Personajes
+https://ghibliapi.herokuapp.com/people
+
+Lugares que aparecen en las películas
+https://ghibliapi.herokuapp.com/locations
+
+ */
+
+const catalogo    = document.querySelector('#catalogo-peliculas');
+
+fetch("https://ghibliapi.herokuapp.com/films")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (peliculas) {
+    console.log(peliculas);
+
+    for( let i = 0; i < peliculas.length; i++) {
+        const pelicula    = document.createElement('article');
+        const titulo      = document.createElement('h2');
+        const descripcion = document.createElement('p');
+        catalogo.appendChild(pelicula);
+        pelicula.appendChild(titulo);
+        pelicula.appendChild(descripcion);
+        
+        titulo.innerHTML      = peliculas[i].title + ' - ' + peliculas[i].release_date;
+        descripcion.innerHTML = peliculas[i].description;
+    }
+  })
+  .catch(function(error) {
+      console.log(error);
+  })
+
+
+
+
